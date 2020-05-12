7db4b25cb9736b069c7c805990c2da9f26141bf2
katpoola <65228755+katpoola@users.noreply.github.com>
2020-05-12 14:59:31 +0300

    kalkulaator v2
    uuendatud versioon

--- kalkulaator.py
+++ kalkulaator.py
@@ -1,11 +1,13 @@
 import easygui
-arv1 = easygui.integerbox("Esimene arv: ", lowerbound = 1, upperbound = 10)
-arv2 = easygui.integerbox("Teine arv: ", lowerbound = 1, upperbound = 10)
-tehted = ["+", "-", "*"]
-valik = easygui.buttonbox("Vali üks kolmest", choices = tehted)
+arv1 = easygui.integerbox("Esimene arv: ", lowerbound = 1, upperbound = 100)
+arv2 = easygui.integerbox("Teine arv: ", lowerbound = 1, upperbound = 100)
+tehted = ["+", "-", "*", ":"]
+valik = easygui.buttonbox("Vali üks neljast", choices = tehted)
 if valik == "+":
     easygui.msgbox(arv1+arv2)
 elif valik == "-":
     easygui.msgbox(arv1-arv2)
-else:
+elif valik == "*":
     easygui.msgbox(arv1*arv2)
+else:
+    easygui.msgbox(arv1//arv2)
